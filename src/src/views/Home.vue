c57dbb0d382afc337e74d77a8bdcdf3580762430
James nolan <jamesnolan1@hotmail.com>
2021-11-16 22:58:42 +0000

    home page html and css

--- src/src/views/Home.vue
+++ src/src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container-fluid">
+  <div class="d-flex justify-content-center">
+    <div
+      style="
+        width: 350px;
+        position: absolute;
+        padding: 10px;
+        text-align: left;
+      "
+    >
+      <h1><b>James Nolan.</b></h1>
+      <h4>I love to code, hire me</h4>
+    </div>
+    <div>
+      <img
+        src="@/assets/Hello.png"
+        style="width: 350px; position: absolute; padding: 10px"
+      />
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      desk: { backgroundImage: "url(@/assests/Desk.png)" },
+    };
+  },
+};
 </script>
+
+<style scoped>
+</style>
